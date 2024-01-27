24fe08fd61631c7415c9dc2534ede2c08dc50c93
priyabrata1122 <priyabratass64@gmail.com>
2024-01-27 19:50:44 +0530

    update day 2

--- script.js
+++ script.js
@@ -20,27 +20,3 @@ const signup_btn = document.getElementById("btn_signup");
 signup_btn.addEventListener('click',function(){
     alert("You are being redirected to an external website.");
 });
-
-// Signup form submission
-document.getElementById('signupForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    // Get signup form data
-    var formData = new FormData(this);
-    // Send signup form data to server for processing
-    // Example: You can use fetch() or XMLHttpRequest to send data to the server
-    console.log('Signup form data:', formData);
-});
-
-// Login form submission
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    // Get login form data
-    var formData = new FormData(this);
-    // Send login form data to server for authentication
-    // Example: You can use fetch() or XMLHttpRequest to send data to the server
-    console.log('Login form data:', formData);
-});
-
-document.getElementById("login_button").addEventListener('click',function(){
-    alert("Welcome");
-});
